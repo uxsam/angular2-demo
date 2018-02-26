12e00398d2dfc7ffc66a0bcfb15093f654d95cc7
uxsam <samm.mathai@gmail.com>
2018-02-26 12:10:17 -0500

    added tap

--- src/app/model/base-data.service.ts
+++ src/app/model/base-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/Operators'
+import { map,tap } from 'rxjs/Operators'
 
 export interface Character {
   id: number,
@@ -17,7 +17,11 @@ export class BaseDataService {
   }
   characterUrl = './api/characters.json';
   getConfig() : Observable<Character[]>  {
-    return this.http.get(this.characterUrl).pipe(
+    return this.http.get<Character>(this.characterUrl).pipe(
+      tap( // Log the result or error
+        data => console.log( data),
+        error => console.log(error)
+      ),
       map((data: any) => {
         return data.data.map(entry=>entry as Character)
       })     
